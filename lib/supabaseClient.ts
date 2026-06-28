7e8628b02a172f59a4adb5bc5fec0be8582c0d9c
kokuden72-dot <kokuden72@gmail.com>
2026-06-28 23:43:03 +0900

    0628alpha3

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://iakiidblfhkftzauetva.supabase.co';
+const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlha2lpZGJsZmhrZnR6YXVldHZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NDM3OTUsImV4cCI6MjA5ODIxOTc5NX0.2PFU_bcTEl_5lwoD00fato1Ii9-a93Orc9pP5rsero0;
 
 export const isSupabaseEnabled = Boolean(supabaseUrl && supabaseAnonKey);
 
